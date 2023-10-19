4a2e52319cd9fff1a114d0b59cb27d063551c8f2
marc-love <marc.love@hmcts.net>
2023-10-19 15:46:03 +0100

    initial commit

---
+++ scripts/dev/CaseList_07/vuser_init.c
@@ -0,0 +1,10 @@
+//   *****************************************************************************************************************************************
+//   ****   PLEASE NOTE: This is a READ-ONLY representation of the actual script. For editing please press the "Develop Script" button.   ****
+//   *****************************************************************************************************************************************
+
+vuser_init()
+{
+	truclient_step("1", "Call Function Common.SignIn", "snapshot=Init_1.inf");
+
+	return 0;
+}
