886b66a189438ee48d545f1eebc089aed03933d8
marc-love <marc.love@hmcts.net>
2023-11-10 19:13:09 +0000

    My Work functionality

--- Scripts/Common/AttachDocuments/vuser_init.c
+++ Scripts/Common/AttachDocuments/vuser_init.c
@@ -7,7 +7,6 @@ vuser_init()
 	/* Resize to improve object recognition */
 	truclient_step("2", "Resize browser window to 1500 px / 1050 px", "snapshot=Init_2.inf");
 	truclient_step("3", "Call Function Common.SignIn", "snapshot=Init_3.inf");
-	truclient_step("4", "Navigate to https://xui-webapp-pr-33...20Correspondence", "snapshot=Init_4.inf");
 
 	return 0;
 }
