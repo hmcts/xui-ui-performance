7ed87b730db4c948d699bf2605ccb82e79b0a239
Dean Cullen <Dean_cullen@next.co.uk>
2024-06-11 11:57:27 +0100

    updated object in myWork

--- Scripts/MyWork/pre_cci.c
+++ Scripts/MyWork/pre_cci.c
@@ -1,4 +1,4 @@
-# 1 "c:\\git\\xui-ui-performance\\scripts\\mywork\\\\combined_MyWork.c"
+# 1 "c:\\users\\dean-\\source\\hmcts\\repos\\xui-ui-performance\\scripts\\mywork\\\\combined_MyWork.c"
 # 1 "C:\\Program Files (x86)\\Micro Focus\\LoadRunner\\include/lrun.h" 1
  
  
@@ -968,7 +968,7 @@ int lr_db_getvalue(char * pFirstArg, ...);
 
 
 
-# 1 "c:\\git\\xui-ui-performance\\scripts\\mywork\\\\combined_MyWork.c" 2
+# 1 "c:\\users\\dean-\\source\\hmcts\\repos\\xui-ui-performance\\scripts\\mywork\\\\combined_MyWork.c" 2
 
 # 1 "C:\\Program Files (x86)\\Micro Focus\\LoadRunner\\include/SharedParameter.h" 1
 
@@ -1136,7 +1136,7 @@ extern VTCERR2  lrvtc_noop();
 
 
 
-# 2 "c:\\git\\xui-ui-performance\\scripts\\mywork\\\\combined_MyWork.c" 2
+# 2 "c:\\users\\dean-\\source\\hmcts\\repos\\xui-ui-performance\\scripts\\mywork\\\\combined_MyWork.c" 2
 
 # 1 "globals.h" 1
 
@@ -1155,7 +1155,7 @@ extern VTCERR2  lrvtc_noop();
 
 # 2 "globals.h" 2
 
-# 3 "c:\\git\\xui-ui-performance\\scripts\\mywork\\\\combined_MyWork.c" 2
+# 3 "c:\\users\\dean-\\source\\hmcts\\repos\\xui-ui-performance\\scripts\\mywork\\\\combined_MyWork.c" 2
 
 # 1 "C-functions.c" 1
  
@@ -1169,5 +1169,5 @@ void main()
 {
 	
 }
-# 4 "c:\\git\\xui-ui-performance\\scripts\\mywork\\\\combined_MyWork.c" 2
+# 4 "c:\\users\\dean-\\source\\hmcts\\repos\\xui-ui-performance\\scripts\\mywork\\\\combined_MyWork.c" 2
 
